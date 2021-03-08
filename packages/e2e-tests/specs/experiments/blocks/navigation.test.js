c0f7ad0a1581b9815c84bccbb699c56c21e7db28
Kerry Liu <kerryliu@gmail.com>
2021-03-08 09:21:16 -0800

    Navigation: re-enable e2e tests

--- packages/e2e-tests/specs/experiments/blocks/navigation.test.js
+++ packages/e2e-tests/specs/experiments/blocks/navigation.test.js
@@ -268,8 +268,7 @@ afterEach( async () => {
 	await setUpResponseMocking( [] );
 } );
 
-// Skip reason: https://github.com/WordPress/gutenberg/issues/27588.
-describe.skip( 'Navigation', () => {
+describe( 'Navigation', () => {
 	describe( 'Creating from existing Pages', () => {
 		it( 'allows a navigation block to be created using existing pages', async () => {
 			// Mock the response from the Pages endpoint. This is done so that the pages returned are always
